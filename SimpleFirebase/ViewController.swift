c712df7183fd8f037eb7b0324f389dcbfe974e0e
ykro <aacs85@gmail.com>
2016-08-18 10:58:40 -0600

    first commit

--- SimpleFirebase/ViewController.swift
+++ SimpleFirebase/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    private let WORDS_CHILD: String = "words"
+    private let MESSAGE_CHILD: String = "message"
 
+    @IBOutlet weak var txtNotice: UITextView!
+    @IBOutlet weak var inputWord: UITextField!
+    @IBAction func didClickedSubmit(sender: UIButton) {
+        if inputWord.text != nil && !inputWord.text!.isEmpty {
+            let wordsReference: FIRDatabaseReference = FIRDatabase.database().reference().child(WORDS_CHILD)
+            let newElementReference = wordsReference.childByAutoId()
+            
+            newElementReference.setValue(inputWord.text)
+            inputWord.text = ""
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let database: FIRDatabase = FIRDatabase.database()
+        let messageReference: FIRDatabaseReference = database.reference().child(MESSAGE_CHILD)
+        messageReference.setValue("Hello World!")
+        messageReference.observeEventType(FIRDataEventType.Value, withBlock: { (snapshot) in
+            guard snapshot.value is String else {
+                print("Error: \(self.MESSAGE_CHILD) value is not a string")
+                return
+            }
+            let msg: String = snapshot.value as! String
+            self.txtNotice.text = msg
+        })
+        
+        let wordsReference: FIRDatabaseReference = FIRDatabase.database().reference().child(WORDS_CHILD)
+        wordsReference.observeEventType(FIRDataEventType.ChildAdded, withBlock: { (snapshot) in
+            let msg: String = snapshot.value as! String
+            self.txtNotice.text = msg
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
